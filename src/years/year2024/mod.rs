563ed9618cbe5b7ab519abaa256b32759dca9b5b
Perodactyl <friedwhoman@gmail.com>
2025-11-08 23:10:50 -0500

    2024 day 11 part 1 (already done previously)

--- src/years/year2024/mod.rs
+++ src/years/year2024/mod.rs
@@ -1,10 +1,12 @@
 mod day6;
 mod day7;
+mod day11;
 
 pub fn puzzle(day: usize, input: String) -> anyhow::Result<String> {
 	match day {
-		6 => day6::main(input),
-		7 => day7::main(input),
+		6  => day6::main(input),
+		7  => day7::main(input),
+		11 => day11::main(input),
 		d => panic!("Day {d} is not existant")
 	}
 }
