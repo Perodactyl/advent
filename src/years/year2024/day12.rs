c07daa4e4759c6c39cdac545e93dc2b9994dd9a8
Perodactyl <friedwhoman@gmail.com>
2025-11-09 14:49:38 -0500

    2024 day 12 part 1 (with files lol)

---
+++ src/years/year2024/day12.rs
@@ -0,0 +1,56 @@
+use std::fmt::Display;
+
+use crate::lib::prelude::*;
+
+#[derive(Debug, Clone, Copy)]
+struct Cell {
+	ch: char,
+	visited: bool,
+} impl From<char> for Cell {
+	fn from(ch: char) -> Cell {
+		Cell {
+			ch,
+			visited: false,
+		}
+	}
+} impl Display for Cell {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+	    write!(f, "{}", if self.visited { self.ch.to_ascii_lowercase() } else { self.ch })
+	}
+}
+
+fn compute_area_and_perim((ch, x, y): (char, usize, usize), grid: &mut impl Grid<Cell>) -> (u32, u32) {
+	let mut area = 1;
+	let mut perim = 4;
+	grid.get_mut(x, y).visited = true;
+
+	for
+		NeighboorhoodMember { abs_x, abs_y, .. }
+		in grid.get_neighborhood(x, y, VON_NEUMANN_NEIGHBORHOOD).of_units()
+	{
+		let item = grid.get(abs_x, abs_y);
+		if item.ch == ch {
+			perim -= 1;
+			if item.visited { continue };
+
+			let (recurse_area, recurse_perim) = compute_area_and_perim((ch, abs_x, abs_y), grid);
+			area += recurse_area;
+			perim += recurse_perim;
+		}
+	}
+	(area, perim)
+}
+
+pub fn main(input: String) -> Result<String> {
+	let mut grid = ItemGrid::<Cell>::from(&input[..]);
+	let mut sum = 0;
+	loop {
+		let Some((start_cell, start_x, start_y)) = grid.find(|c, _, _| c.visited == false) else { break };
+		let ch = start_cell.ch;
+		let (area, perimeter) = compute_area_and_perim((ch, start_x, start_y), &mut grid);
+		println!("{ch}: {area} * {perimeter} = {}", area * perimeter);
+		sum += area * perimeter;
+	}
+
+	Ok(format!("{sum}"))
+}
